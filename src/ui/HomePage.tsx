f8e039d90852b4a7ed20473698b2a8557438ba94
talza <24567980+TalZa1810@users.noreply.github.com>
2025-12-21 10:22:51 +0200

    table & cards view

--- src/ui/HomePage.tsx
+++ src/ui/HomePage.tsx
@@ -1,8 +1,17 @@
 import {MyopComponent} from "@myop/react";
 import {COMPONENTS_IDS} from "../utils/componentsIds.ts";
-
+import {useState} from "react";
 
 export const HomePage = () => {
+
+    const [view, setView] = useState('table')
+
+
+    const handleCta = (action: string, payload: any) => {
+        if (action === 'view-changed' && payload) {
+            setView(payload.view);
+        }
+    };
 
     return  <div style={{ flex: 1, display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
         {/* Header Insights */}
@@ -10,9 +19,19 @@ export const HomePage = () => {
             <MyopComponent componentId={COMPONENTS_IDS.headerInsights} />
         </div>
 
-        {/* Table */}
+        {/* Content Header */}
+        <div style={{  padding: '0 24px 24px' }} >
+            <MyopComponent
+                componentId={COMPONENTS_IDS.tableHeader}
+                on={handleCta as any}
+            />
+
+        </div>
         <div style={{ flex: 1, overflow: 'auto', padding: '0 24px 24px' }}>
-            <MyopComponent componentId={COMPONENTS_IDS.table} />
+            { view === 'table' ?
+                <MyopComponent componentId={COMPONENTS_IDS.table} />:
+                <MyopComponent componentId={COMPONENTS_IDS.cardsView} />
+            }
         </div>
     </div>
 }
--- src/utils/componentsIds.ts
+++ src/utils/componentsIds.ts
@@ -4,5 +4,6 @@ export const COMPONENTS_IDS = {
     headerInsights: '6eb6a000-fc12-4166-aba8-dc0f5c17f041', //headerInsights.html
     sidebar: 'c393d4be-4899-4655-8b92-d52ad1a6537d', //sidebar.html
     cardsView: '46ecb98c-997c-4dc4-97f7-3c7fddc186d6', //cardsView.html
-    profilePopover: '62d87571-6985-4a1b-9ced-53374f89da05'
+    profilePopover: '62d87571-6985-4a1b-9ced-53374f89da05', //profilePopover.html
+    tableHeader: '5a57bdf8-14b0-4089-93f1-cdccdf861c95',
 }
